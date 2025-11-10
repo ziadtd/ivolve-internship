4c8f39d78da20bcbf6fd8ffe6b681a8a212d023d
ziadtd <141604127+ziadtd@users.noreply.github.com>
2025-11-10 08:52:33 +0200

    Update deployOnK8s.groovy

--- jenkins/Task31-PipelineWithAgentsAndSharedLib/jenkins_shared_lib/vars/deployOnK8s.groovy
+++ jenkins/Task31-PipelineWithAgentsAndSharedLib/jenkins_shared_lib/vars/deployOnK8s.groovy
@@ -2,7 +2,7 @@ def call() {
     echo "Deploying application on Kubernetes"
                 withCredentials([file(credentialsId: 'kube-config', variable: 'KUBECONFIG')]) {
                     sh '''
-                    kubectl apply -f jenkins/Task31-PipelineWithAgentsAndSharedLib/Jdeployment.yaml --kubeconfig=$KUBECONFIG
+                    kubectl apply -f jenkins/Task31-PipelineWithAgentsAndSharedLib/deployment.yaml --kubeconfig=$KUBECONFIG
                     kubectl rollout status deployment/myapp --kubeconfig=$KUBECONFIG
                     '''
                 }            
